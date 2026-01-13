f071c1134300e5cbf835f275d6726ffa7e38a41d
hrithik-anotech <hrithik.gupta@anotechsolutions.com>
2026-01-13 18:40:09 +0530

    deploy2

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://mern-investent.netlify.app"
+    "https://mern-investment.netlify.app"
   ],
   credentials: true
 }));
