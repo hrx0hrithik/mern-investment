ab001e532c1a3d3a0834470307393413b9492b5a
hrithik-anotech <hrithik.gupta@anotechsolutions.com>
2026-01-13 18:28:44 +0530

    deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-frontend.vercel.app"
+    "https://mern-investent.netlify.app"
   ],
   credentials: true
 }));
