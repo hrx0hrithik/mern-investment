683d10c149d17183fcc8f9a566ff968769f3f6eb
hrithik-anotech <hrithik.gupta@anotechsolutions.com>
2026-01-15 17:50:35 +0530

    dashboard

--- backend/src/controllers/dashboard.controller.js
+++ backend/src/controllers/dashboard.controller.js
@@ -1,25 +1,26 @@
-import Investment from "../models/Investment.model.js"
-import ROIHistory from "../models/ROIHistory.model.js"
-import LevelIncome from "../models/LevelIncome.model.js"
+import mongoose from "mongoose";
+import Investment from "../models/Investment.model.js";
+import ROIHistory from "../models/ROIHistory.model.js";
+import LevelIncome from "../models/LevelIncome.model.js";
 
 export const getDashboard = async (req, res) => {
-  const userId = req.user.id
+  const userId = new mongoose.Types.ObjectId(req.user.id);
 
-  const investments = await Investment.find({ user: userId })
+  const investments = await Investment.find({ user: userId });
 
   const totalROI = await ROIHistory.aggregate([
     { $match: { user: userId } },
     { $group: { _id: null, total: { $sum: "$roiAmount" } } },
-  ])
+  ]);
 
   const levelIncome = await LevelIncome.aggregate([
     { $match: { user: userId } },
     { $group: { _id: null, total: { $sum: "$amount" } } },
-  ])
+  ]);
 
   res.json({
     investments,
     totalROI: totalROI[0]?.total || 0,
     levelIncome: levelIncome[0]?.total || 0,
-  })
-}
+  });
+};
